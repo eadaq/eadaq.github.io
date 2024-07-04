91f247e7206468bf58de15a4a94f353d9ec58241
eada <finn.larsson@protonmail.ch>
2024-07-04 21:55:06 +0200

    Add files via upload

---
+++ game_atp.js
@@ -0,0 +1,58 @@
+document.write('\
+			<div class="container">\
+				<div class="kopfzeile">\
+					<div style="display: inline;"><span class="meta"> · </span><span style=\'opacity: .54;\'></span></div>\
+					<div style="position: relative; float: right;"><span class="zeit">Noch offen</span></div>\
+				</div>\
+				<div>\
+					<table>\
+						<colgroup>\
+							<col style="width: 25px;">\
+							<col style="width: 25px;">\
+							<col style="width: 100%;">\
+							\
+							<col style="width: 25px;">\
+							<col style="width: 25px;">\
+							<col style="width: 25px;">\
+							<col style="width: 25px;">\
+							<col style="width: 25px;">\
+							\
+							<col style="width: 0px;">\
+						</colgroup>\
+						<tbody>\
+							<tr>\
+								<th><div style=\'box-sizing: border-box;\'>\
+										<img class="src" style="vertical-align: top;" alt=\'\' height=\'25\' src=\'\'>\
+								</div></th>\
+								<td class=\'td-space\'><span class="seed"></span></td>\
+								<td class=\'td-text\'><span class="player span-text">TBD</span></div></td>\
+								\
+								<td><div><p contenteditable=\'True\' data-placeholder=\'·\'></p></div></td>\
+								<td><div><p contenteditable=\'True\' data-placeholder=\'·\'></p></div></td>\
+								<td><div><p contenteditable=\'True\' data-placeholder=\'·\'></p></div></td>\
+								<td><div><p contenteditable=\'True\' data-placeholder=\'·\'></p></div></td>\
+								<td><div><p contenteditable=\'True\' data-placeholder=\'·\'></p></div></td>\
+								\
+								<td class=\'td-space\'></td>\
+							</tr>		    	\
+							<tr>\
+								<th><div style=\'box-sizing: border-box;\'>\
+										<img class="src" style="vertical-align: top;" alt=\'\' height=\'25\' src=\'\'>\
+								</div></th>\
+								<td class=\'td-space\'><span class="seed"></span></td>\
+								<td class=\'td-text\'><span class="player span-text">TBD</span></div></td>\
+								\
+								<td><div><p contenteditable=\'True\' data-placeholder=\'·\'></p></div></td>\
+								<td><div><p contenteditable=\'True\' data-placeholder=\'·\'></p></div></td>\
+								<td><div><p contenteditable=\'True\' data-placeholder=\'·\'></p></div></td>\
+								<td><div><p contenteditable=\'True\' data-placeholder=\'·\'></p></div></td>\
+								<td><div><p contenteditable=\'True\' data-placeholder=\'·\'></p></div></td>\
+								\
+								<td class=\'td-space\'></td>\
+							</tr>		\
+						</tbody>\
+					</table>\
+				</div>\
+			</div>	\
+\
+');
